d45b7d4552506ce617fc35bd89d072f8df8eba77
Testtsts <ihsannur0610@gmail.com>
2024-06-03 09:34:04 +0700

    simple crud app NRP name

---
+++ app/src/main/java/com/example/myapplication/NrpSearchDialog.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class NrpSearchDialog extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.dialog_layout,null))
+                .setPositiveButton("Search", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        NrpSearchDialog.this.getDialog().cancel();
+                    }
+                });
+
+
+        return builder.create();
+    }
+}
